cb92e0577c819011b65acae693882137ed881edc
Waldemar Kornewald <wkornewald@gmail.com>
2020-04-10 23:43:54 +0200

    Added lifecycle observers for ON_RESUME/ON_PAUSE

--- reactivestate/src/main/java/com/ensody/reactivestate/Lifecycle.kt
+++ reactivestate/src/main/java/com/ensody/reactivestate/Lifecycle.kt
@@ -131,17 +131,100 @@ fun LifecycleOwner.onStop(block: () -> Unit): Disposable =
 fun LifecycleOwner.onStopOnce(block: () -> Unit): Disposable =
     addObserver(OnStopObserver(lifecycle, true, block))
 
+private class OnResumeObserver(
+    lifecycle: Lifecycle,
+    private val once: Boolean,
+    private val block: () -> Unit
+) : DisposableObserver(lifecycle) {
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private fun handle() {
+        block()
+        if (once) {
+            dispose()
+        }
+    }
+}
+
+/**
+ * Runs the given block on every [Lifecycle.Event.ON_RESUME].
+ *
+ * @return [Disposable] that allows removing the observer.
+ * */
+fun LifecycleOwner.onResume(block: () -> Unit): Disposable =
+    addObserver(OnResumeObserver(lifecycle, false, block))
+
+/**
+ * Runs the given block once on the next [Lifecycle.Event.ON_RESUME].
+ *
+ * @return [Disposable] that allows removing the observer.
+ * */
+fun LifecycleOwner.onResumeOnce(block: () -> Unit): Disposable =
+    addObserver(OnResumeObserver(lifecycle, true, block))
+
+private class OnPauseObserver(
+    lifecycle: Lifecycle,
+    private val once: Boolean,
+    private val block: () -> Unit
+) : DisposableObserver(lifecycle) {
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private fun handle() {
+        block()
+        if (once) {
+            dispose()
+        }
+    }
+}
+
+/**
+ * Runs the given block on every [Lifecycle.Event.ON_PAUSE].
+ *
+ * @return [Disposable] that allows removing the observer.
+ * */
+fun LifecycleOwner.onPause(block: () -> Unit): Disposable =
+    addObserver(OnPauseObserver(lifecycle, false, block))
+
+/**
+ * Runs the given block once on the next [Lifecycle.Event.ON_PAUSE].
+ *
+ * @return [Disposable] that allows removing the observer.
+ * */
+fun LifecycleOwner.onPauseOnce(block: () -> Unit): Disposable =
+    addObserver(OnPauseObserver(lifecycle, true, block))
+
 /**
  * Launches and runs the given block when and as long as the [Lifecycle] controlling this
  * [LifecycleCoroutineScope] is at least in the [Lifecycle.State.STARTED] state.
  *
+ * This is useful for e.g. processing a `Flow` or `Channel` of events only while the Fragment is
+ * started.
+ *
  * The returned [Job] will be canceled when the [Lifecycle] is stopped ([Lifecycle.Event.ON_STOP]).
+ * This means you should usually call this function in e.g. `Fragment.onStart()`.
  */
 fun LifecycleOwner.launchWhileStarted(block: suspend CoroutineScope.() -> Unit): Job {
     val job = lifecycleScope.launchWhenStarted {
         block()
     }
     val disposable = onStopOnce { job.cancel() }
+    job.invokeOnCompletion { disposable.dispose() }
+    return job
+}
+
+/**
+ * Launches and runs the given block when and as long as the [Lifecycle] controlling this
+ * [LifecycleCoroutineScope] is at least in the [Lifecycle.State.RESUMED] state.
+ *
+ * This is useful for e.g. processing a `Flow` or `Channel` of events only while the Fragment is
+ * resumed.
+ *
+ * The returned [Job] will be canceled when the [Lifecycle] is paused ([Lifecycle.Event.ON_PAUSE]).
+ * This means you should usually call this function in e.g. `Fragment.onResume()`.
+ */
+fun LifecycleOwner.launchWhileResumed(block: suspend CoroutineScope.() -> Unit): Job {
+    val job = lifecycleScope.launchWhenResumed {
+        block()
+    }
+    val disposable = onPauseOnce { job.cancel() }
     job.invokeOnCompletion { disposable.dispose() }
     return job
 }
--- reactivestate/src/test/java/com/ensody/reactivestate/LifecycleTest.kt
+++ reactivestate/src/test/java/com/ensody/reactivestate/LifecycleTest.kt
@@ -19,9 +19,13 @@ class LifecycleTest {
 
         var run = 0
         var start = 0
-        var stop = 0
         var startOnce = 0
+        var stop = 0
         var stopOnce = 0
+        var resume = 0
+        var resumeOnce = 0
+        var pause = 0
+        var pauseOnce = 0
 
         owner.autoRun {
             run += 1
@@ -29,55 +33,127 @@ class LifecycleTest {
         owner.onStart {
             start += 1
         }
-        owner.onStop {
-            stop += 1
-        }
         owner.onStartOnce {
             startOnce += 1
+        }
+        owner.onStop {
+            stop += 1
         }
         owner.onStopOnce {
             stopOnce += 1
         }
+        owner.onResume {
+            resume += 1
+        }
+        owner.onResumeOnce {
+            resumeOnce += 1
+        }
+        owner.onPause {
+            pause += 1
+        }
+        owner.onPauseOnce {
+            pauseOnce += 1
+        }
 
         assertThat(run).isEqualTo(0)
         assertThat(start).isEqualTo(0)
-        assertThat(stop).isEqualTo(0)
         assertThat(startOnce).isEqualTo(0)
-        assertThat(stopOnce).isEqualTo(0)
-
-        owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
-        assertThat(run).isEqualTo(1)
-        assertThat(start).isEqualTo(1)
         assertThat(stop).isEqualTo(0)
-        assertThat(startOnce).isEqualTo(1)
         assertThat(stopOnce).isEqualTo(0)
+        assertThat(resume).isEqualTo(0)
+        assertThat(resumeOnce).isEqualTo(0)
+        assertThat(pause).isEqualTo(0)
+        assertThat(pauseOnce).isEqualTo(0)
+
+        owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
+        assertThat(run).isEqualTo(1)
+        assertThat(start).isEqualTo(1)
+        assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(0)
+        assertThat(stopOnce).isEqualTo(0)
+        assertThat(resume).isEqualTo(0)
+        assertThat(resumeOnce).isEqualTo(0)
+        assertThat(pause).isEqualTo(0)
+        assertThat(pauseOnce).isEqualTo(0)
 
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        assertThat(run).isEqualTo(1)
+        assertThat(start).isEqualTo(1)
+        assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(0)
+        assertThat(stopOnce).isEqualTo(0)
+        assertThat(resume).isEqualTo(1)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(0)
+        assertThat(pauseOnce).isEqualTo(0)
+
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        assertThat(run).isEqualTo(1)
+        assertThat(start).isEqualTo(1)
+        assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(0)
+        assertThat(stopOnce).isEqualTo(0)
+        assertThat(resume).isEqualTo(1)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(1)
+        assertThat(pauseOnce).isEqualTo(1)
+
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
         assertThat(run).isEqualTo(1)
         assertThat(start).isEqualTo(1)
-        assertThat(stop).isEqualTo(1)
         assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(1)
         assertThat(stopOnce).isEqualTo(1)
+        assertThat(resume).isEqualTo(1)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(1)
+        assertThat(pauseOnce).isEqualTo(1)
 
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
         assertThat(run).isEqualTo(1)
         assertThat(start).isEqualTo(2)
-        assertThat(stop).isEqualTo(1)
         assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(1)
         assertThat(stopOnce).isEqualTo(1)
+        assertThat(resume).isEqualTo(1)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(1)
+        assertThat(pauseOnce).isEqualTo(1)
 
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        assertThat(run).isEqualTo(1)
+        assertThat(start).isEqualTo(2)
+        assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(1)
+        assertThat(stopOnce).isEqualTo(1)
+        assertThat(resume).isEqualTo(2)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(1)
+        assertThat(pauseOnce).isEqualTo(1)
+
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        assertThat(run).isEqualTo(1)
+        assertThat(start).isEqualTo(2)
+        assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(1)
+        assertThat(stopOnce).isEqualTo(1)
+        assertThat(resume).isEqualTo(2)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(2)
+        assertThat(pauseOnce).isEqualTo(1)
+
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
         assertThat(run).isEqualTo(1)
         assertThat(start).isEqualTo(2)
-        assertThat(stop).isEqualTo(2)
         assertThat(startOnce).isEqualTo(1)
+        assertThat(stop).isEqualTo(2)
         assertThat(stopOnce).isEqualTo(1)
+        assertThat(resume).isEqualTo(2)
+        assertThat(resumeOnce).isEqualTo(1)
+        assertThat(pause).isEqualTo(2)
+        assertThat(pauseOnce).isEqualTo(1)
 
         owner.lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        assertThat(owner.lifecycle.observerCount).isEqualTo(2)
+        assertThat(owner.lifecycle.observerCount).isEqualTo(4)
     }
 }
