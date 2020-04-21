10672e5a91d29760541e8dc3067437cf2eb92d9d
Waldemar Kornewald <wkornewald@gmail.com>
2020-04-21 09:27:27 +0200

    Deploy docs with SSH key

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -12,7 +12,10 @@ if [ -n "${GITHUB_REPOSITORY:-}" ] && ! git config --get user.name; then
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git remote rm origin
-  git remote add origin "https://x-access-token:${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git remote add origin "git@github.com:${GITHUB_REPOSITORY}.git"
+  eval "$(ssh-agent)"
+  ssh-add <(echo "$GH_PAGES_DEPLOY_KEY")
+  trap "ssh-agent -k" EXIT
 fi
 
 mkdocs gh-deploy --config-file "$ROOT/mkdocs.yml" --force
