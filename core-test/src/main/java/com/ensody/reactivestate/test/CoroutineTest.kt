26024e88bff800a1f23ee5a55280390d4b30a40b
Waldemar Kornewald <wkornewald@gmail.com>
2020-11-15 19:42:54 +0100

    Added helpers for unit tests

---
+++ core-test/src/main/java/com/ensody/reactivestate/test/CoroutineTest.kt
@@ -0,0 +1,22 @@
+package com.ensody.reactivestate.test
+
+import com.ensody.reactivestate.dispatchers
+import kotlinx.coroutines.test.TestCoroutineScope
+import org.junit.Rule
+
+/**
+ * Base class for unit testing coroutine based code.
+ *
+ * On every test run this class automatically sets [dispatchers] to the [TestCoroutineDispatcherConfig].
+ *
+ * Internally, this uses a [CoroutineTestRule] which does the actual [dispatchers] setup.
+ * You can access the rule via [coroutinesTestRule] in order to e.g. use the [TestCoroutineScope] in your
+ * `@Before` setup method.
+ */
+public abstract class CoroutineTest {
+    @get:Rule
+    public var coroutinesTestRule: CoroutineTestRule = CoroutineTestRule()
+
+    public open fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit): Unit =
+        coroutinesTestRule.runBlockingTest(block)
+}
