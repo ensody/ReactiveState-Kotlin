766507ba4a50192147065cd72126d85ef85c4677
Waldemar Kornewald <wkornewald@gmail.com>
2021-03-09 16:19:11 +0100

    Fixed BOM

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -17,8 +17,4 @@ apt-get install -y --no-install-recommends ca-certificates curl gettext git pyth
 ln -sf "$(command -v python3)" /usr/bin/python
 
 # Install poetry
-POETRY_VERSION="1.0.5"
-download "https://raw.githubusercontent.com/sdispater/poetry/${POETRY_VERSION}/get-poetry.py" \
-  40cf7b39a926acae9d1d0293983a508c58a6788554c6120a84e65d18ce044e51 /tmp/get-poetry.py
-python3 /tmp/get-poetry.py -y --version "$POETRY_VERSION"
-rm -rf /tmp/get-poetry.py
+pip3 install "poetry==1.0.10"
