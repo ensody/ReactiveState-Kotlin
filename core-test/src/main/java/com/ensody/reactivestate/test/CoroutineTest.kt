e77ca15ad158deb54fe1b9c188b18fa21755dd1a
Waldemar Kornewald <wkornewald@ensody.com>
2021-03-15 17:59:05 +0100

    Enhanced CoroutineTest with AttachedDisposables and collectFlow

--- core-test/src/main/java/com/ensody/reactivestate/test/CoroutineTest.kt
+++ core-test/src/main/java/com/ensody/reactivestate/test/CoroutineTest.kt
@@ -1,6 +1,13 @@
 package com.ensody.reactivestate.test
 
+import com.ensody.reactivestate.AttachedDisposables
+import com.ensody.reactivestate.DisposableGroup
+import com.ensody.reactivestate.derived
 import com.ensody.reactivestate.dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestCoroutineScope
 import org.junit.Rule
 
@@ -12,8 +19,29 @@ import org.junit.Rule
  * Internally, this uses a [CoroutineTestRule] which does the actual [dispatchers] and `Dispatchers.setMain()` setup.
  * You can access the rule via [coroutineTestRule] in order to e.g. use the [TestCoroutineScope] in your
  * `@Before` setup method.
+ *
+ * Moreover, this provides an [attachedDisposables] attribute and a [collectFlow] helper, so you can activate
+ * [SharingStarted.WhileSubscribed] based flows created with [derived], for example.
  */
-public open class CoroutineTest : CoroutineTestRuleOwner {
+public open class CoroutineTest : CoroutineTestRuleOwner, AttachedDisposables {
     @get:Rule
     public override val coroutineTestRule: CoroutineTestRule = CoroutineTestRule()
+
+    override val attachedDisposables: DisposableGroup = DisposableGroup()
+
+    /** Collects a flow in the background. */
+    public fun <T> TestCoroutineScope.collectFlow(flow: Flow<T>, collector: suspend (T) -> Unit = {}) {
+        attachedDisposables.add(launch { flow.collect(collector) })
+    }
+
+    /** In addition to running the test this also disposes the [attachedDisposables] (useful with [collectFlow]). */
+    override fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) {
+        super.runBlockingTest {
+            try {
+                block()
+            } finally {
+                dispose()
+            }
+        }
+    }
 }
