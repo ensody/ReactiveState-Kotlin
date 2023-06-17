4340a1bb63b400a8d8760776826d830073d06ddb
Waldemar Kornewald <wkornewald@ensody.com>
2023-06-17 07:27:16 +0200

    Logs

--- reactivestate/src/androidUnitTest/kotlin/com/ensody/reactivestate/android/FragmentTest.kt
+++ reactivestate/src/androidUnitTest/kotlin/com/ensody/reactivestate/android/FragmentTest.kt
@@ -6,6 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ensody.reactivestate.test.AndroidCoroutineTest
 import kotlinx.coroutines.test.runCurrent
 import org.junit.runner.RunWith
+import java.time.Instant
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
@@ -16,31 +17,46 @@ import kotlin.test.assertSame
 internal class FragmentTest : AndroidCoroutineTest() {
     @Test
     fun creationOfReactiveState() = runTest {
+        println("${Instant.now()} launchFragmentInContainer")
         val scenario = launchFragmentInContainer<TestFragment>()
+        println("${Instant.now()} moveToState")
         scenario.moveToState(Lifecycle.State.RESUMED)
         lateinit var viewModel: TestViewModel
         lateinit var fragment: TestFragment
+        println("${Instant.now()} onFragment")
         scenario.onFragment {
+            println("${Instant.now()} within onFragment")
             fragment = it
             viewModel = it.viewModel
             it.viewModel.count.value = 10
+            println("${Instant.now()} within onFragment runCurrent")
             runCurrent()
+            println("${Instant.now()} within onFragment throwError")
             it.viewModel.throwError()
+            println("${Instant.now()} within onFragment throwError runCurrent")
             runCurrent()
+            println("${Instant.now()} within onFragment assertions")
             assertIs<TestException>(it.errors.first())
             assertEquals(listOf<Any>(it.viewModel), it.attachedReactiveStates)
             assertSame(it.viewModel, it.attachedReactiveStates.first())
+            println("${Instant.now()} within onFragment done")
         }
+        println("${Instant.now()} after onFragment runCurrent")
         runCurrent()
+        println("${Instant.now()} scenario.recreate")
         scenario.recreate()
+        println("${Instant.now()} after scenario runCurrent")
         runCurrent()
+        println("${Instant.now()} scenario.onFragment")
         scenario.onFragment {
+            println("${Instant.now()} within scenario.onFragment")
             assertNotSame(fragment, it)
             assertSame(viewModel, it.viewModel)
             assertEquals(10, it.viewModel.count.value)
             assertEquals(emptyList(), it.errors)
             assertEquals(listOf<Any>(it.viewModel), it.attachedReactiveStates)
             assertSame(it.viewModel, it.attachedReactiveStates.first())
+            println("${Instant.now()} within scenario.onFragment done")
         }
     }
 }
